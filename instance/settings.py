909d7bcc0c94c545a39982a1a9c7a8732d70cb78
srinath <srinath4223@gmail.com>
2021-01-01 14:36:40 +0530

    Updated  Dockrfilee

---
+++ instance/settings.py
@@ -0,0 +1,15 @@
+'''
+app_id=settings.PUSHER_APP_ID,
+    key=settings.PUSHER_KEY,
+    secret=settings.PUSHER_SECRET,
+    cluster=settings.PUSHER_CLUSTER,
+    ssl=settings.PUSHER_SSL
+'''
+#PUSHER_APP_ID
+#PUSHER_KEY
+#PUSHER_SECRET
+#PUSHER_CLUSTER
+#PUSHER_SSL
+
+
+
